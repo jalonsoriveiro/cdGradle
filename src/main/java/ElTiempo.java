a547e8f18d48581c7abc629c4f4ab6590ab69a9e
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2020-02-07 14:26:22 +0100

    Java jsop

--- src/main/java/ElTiempo.java
+++ src/main/java/ElTiempo.java
@@ -7,11 +7,11 @@ public class ElTiempo {
     public static void main(String[]args) throws IOException {
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
         Elements miTemperatura = doc.getElementsByClass("today_nowcard-temp");
-        val textoTemperatura;//= miTemperatura.text();
         Elements miEstadoNu = doc.getElementsByClass("today_nowcard-phrase");
-        val textoTiempo = miEstadoNu.text();
         Elements miEstadoTiempo = doc.getElementsByClass("today_nowcard-feels");
-
-
+        String Titulo = doc.title();
+        String Temperatura = miTemperatura.text();
+        String TIempo = miEstadoTiempo.text();
+        System.out.println("Titulo "+Titulo + "Temperatura "+Temperatura+" Tiempo"+TIempo);
     }
 }
